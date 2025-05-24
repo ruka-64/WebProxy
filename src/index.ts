7ee7461a271a4ceb89dfa874132cc23e4af0db8b
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-05-24 16:19:26 +0900

    config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,76 @@
 import { Context, Hono } from 'hono';
 import { proxy } from 'hono/proxy';
+import { PORT } from '../config';
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const app = new Hono();
 
-const baseUrl = 'https://hono.dev';
+let baseUrl = 'https://ruka64.dev';
+
+let isRunning = false;
+
+const maxTimeSec = 120;
+let remain = 0;
+
+interface WebProxyPostT {
+  url: string;
+}
+
+async function timer() {
+  if (remain === 0) return;
+  while (remain !== 0) {
+    // console.log('[Dev] idle timer', remain);
+    remain--;
+    await delay(1000);
+  }
+  isRunning = false;
+  return;
+}
+
+app.post('/webproxy', async (c) => {
+  const body = (await c.req.json()) as WebProxyPostT;
+  if (!body) {
+    return c.json(
+      {
+        success: false,
+        message: 'Invalid body',
+      },
+      400
+    );
+  }
+  if (isRunning) {
+    return c.json(
+      {
+        success: false,
+        message: 'This node is busy',
+      },
+      400
+    );
+  }
+  const url = new URL(body.url);
+  console.log(
+    `[${new Date().toLocaleString}] Proxy started. target: ${url.origin}`
+  );
+  isRunning = true;
+  baseUrl = url.origin;
+  remain = maxTimeSec;
+  timer();
+  return c.json({
+    success: true,
+    message: 'OK',
+  });
+});
 
 app.all('*', async (c: Context) => {
-  console.log('called');
+  if (c.req.path === '/webproxy' && c.req.method === 'POST') {
+    return;
+  }
+  if (!isRunning) {
+    c.res.headers.set('Cache-Control', 'max-age=0');
+    return c.redirect('https://ruka64.dev/', 301);
+  }
+  remain = maxTimeSec;
   const urlObj = new URL(c.req.url);
   const pathNameAndQuery = `${urlObj.pathname}${urlObj.search}`;
   const method = c.req.method;
@@ -16,20 +80,16 @@ app.all('*', async (c: Context) => {
   // headers.set('accept-encoding', 'gzip');
   const body = c.req.raw.body;
   // console.log('headers', headers);
-  console.log('body', body);
   const buildedUrl = `${baseUrl}${pathNameAndQuery}`;
-  console.log('requesting', buildedUrl);
   const _response = await proxy(buildedUrl, {
     method,
     headers,
     body,
   });
 
-  console.log('encoding', _response.headers.get('content-encoding'));
   // console.log('body', _response.body);
 
   const response = new Response(_response.body, _response);
-  console.log('done');
 
   response.headers.set('access-control-allow-origin', '*');
 
@@ -37,11 +97,13 @@ app.all('*', async (c: Context) => {
   if (redirectDetect && redirectDetect.includes(baseUrl)) {
     response.headers.set('Location', redirectDetect.replace(baseUrl, ''));
   }
+  response.headers.set('Cache-Control', 'max-age=120');
 
   return response as unknown as Response;
 });
 
 export default {
+  port: PORT ?? 3000,
   //@ts-ignore
   async fetch(request, env, ctx): Promise<Response> {
     const urlObj = new URL(request.url);
